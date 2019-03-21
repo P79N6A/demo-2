77c97e302777e7789c84262b69deb6b0a445601e
zhaozi <zhaozi.lyh@antfin.com>
2019-03-21 17:59:46 +0800

    add code

---
+++ src/test/java/com/example/demo/ATest.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author zhaozi on 2019/3/21
+ */
+public class ATest {
+
+    @Test
+    public void shouldASuccess() {
+        Assert.assertEquals(1, 1);
+    }
+
+    @Test
+    public void shouldBSuccess() {
+        Assert.assertEquals(1, 1);
+    }
+}
